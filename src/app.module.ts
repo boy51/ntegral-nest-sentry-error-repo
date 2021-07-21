bbc0577eabf5f5eac86cec03f427a128534747b0
rgoedel <Richard.Goedel.contractor@rle.de>
2021-07-21 17:27:01 +0200

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { SentryInterceptor, SentryModule } from '@ntegral/nestjs-sentry';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    SentryModule.forRoot({
+      dsn: 'https://45740e3ae4864e77a01ad61a47ea3b7e@o115888.ingest.sentry.io/25956308132020',
+      tracesSampleRate: 1.0,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    { provide: APP_INTERCEPTOR, useClass: SentryInterceptor },
+  ],
+})
+export class AppModule {}
